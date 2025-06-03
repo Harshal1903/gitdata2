43540ae9b898c096530210531403d7247fc93c2b
Harshal <harshmahale02@gmail.com>
2025-06-03 17:16:29 +0530

    ArrayList code added

---
+++ SampleTestDemo/src/com/test/SampleTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.util.ArrayList;
+
+public class SampleTest {
+	
+public static void main(String[] args) {
+	
+	ArrayList<Integer> al = new ArrayList<>();
+	al.add(20);
+	al.add(25);
+	al.add(30);
+	al.add(40);
+	al.add(60);
+	al.add(100);
+	System.out.println(al);
+
+}
+	
+}
